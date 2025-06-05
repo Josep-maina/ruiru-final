69fc1e4a5b3b963366f913c763b5737d6067bfdf
Josep-maina <123233319+Josep-maina@users.noreply.github.com>
2025-06-05 09:25:52 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "log"
+    "net/http"
+    "os"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w, "Welcome to Ruiru Final App!")
+}
 
 func main() {
-    fmt.Println("Hello, Ruiru Final App!")
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "8080" // fallback for local testing
+    }
+
+    http.HandleFunc("/", handler)
+
+    log.Printf("Starting server on port %s...\n", port)
+    err := http.ListenAndServe(":"+port, nil)
+    if err != nil {
+        log.Fatal(err)
+    }
 }
